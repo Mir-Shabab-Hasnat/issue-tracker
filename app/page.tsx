b221e31d1bdc4806e8c6d3c127d85a093d8cc4f9
Mir Shabab Hasnat <shababhasnat@gmail.com>
2024-08-31 02:29:12 -0600

    imlemented issue summary

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,14 @@
+import prisma from "@/prisma/client";
 import Pagination from "./components/Pagination";
+import IssueSummary from "./IssueSummary";
 import LatestIssues from "./LatestIssues";
 
-export default function Home() {
-  return <LatestIssues />;
+export default async function Home() {
+  const open = await prisma.isssue.count({ where: { status: "OPEN" } });
+  const closed = await prisma.isssue.count({ where: { status: "CLOSED" } });
+  const inProgress = await prisma.isssue.count({
+    where: { status: "IN_PROGRESS" },
+  });
+
+  return <IssueSummary open={open} inProgress={inProgress} closed={closed} />;
 }
